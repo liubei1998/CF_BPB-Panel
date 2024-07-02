ae94d7f2ccd66619a746b789abad4c98e777a407
yeyepaodecha <118088375+liubei1998@users.noreply.github.com>
2024-07-02 10:09:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -824,17 +824,17 @@ const generateRemark = (index, port) => {
     let remark = '';
     switch (index) {
         case 0:
-        case 1:
+        case 3:
             remark = `💦 BPB - Domain_${index + 1} : ${port}`;
             break;
-        case 2:
-        case 3:
+        case 4:
+        case 6:
             remark = `💦 BPB - IPv4_${index - 1} : ${port}`;
             break;
-        case 4:
-        case 5:
-            remark = `💦 BPB - IPv6_${index - 3} : ${port}`;
-            break;
+        // case 4:
+        // case 5:
+        //     remark = `💦 BPB - IPv6_${index - 3} : ${port}`;
+        //     break;
         default:
             remark = `💦 BPB - Clean IP_${index - 5} : ${port}`;
             break;
